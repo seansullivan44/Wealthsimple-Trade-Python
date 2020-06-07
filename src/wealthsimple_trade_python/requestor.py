9229124531ab217e0d3aab925abc8f5af84d0524
Sean Sullivan <seansullivan3@cmail.carleton.ca>
2020-06-07 17:39:28 -0400

    Add additional docstrings to APIRequestor and WSTrade classes

--- src/wealthsimple_trade_python/requestor.py
+++ src/wealthsimple_trade_python/requestor.py
@@ -2,12 +2,55 @@ import requests
 
 
 class APIRequestor:
+    """
+    A class to simplify request calls to REST API
+
+    Attributes
+    ----------
+    session : session
+        A requests Session object to be associated with the class
+    APIMainURL : str
+        Main URL endpoint for API
+
+    Methods
+    -------
+    makeRequest(method, endpoint, params=None, returnValue=None)
+        Make a request to a given API endpoint
+    post(URL, params=None)
+        Make a POST request to a given API endpoint
+    get(URL, params=None)
+        Make a GET request to a given API endpoint
+    """
+
     def __init__(self, session, APIMainURL):
+        """
+        Parameters
+        ----------
+        session : Session
+            A requests Session object to be associated with the class
+        APIMainURL : str
+            Main URL endpoint for API
+        """
         self.session = session
         self.APIMainURL = APIMainURL
-        self.session.auth = ("username", "yolo")
 
     def makeRequest(self, method, endpoint, params=None, returnValue=None):
+        """Make a request to a given API endpoint
+
+        Parameters
+        ----------
+        method : str
+            Specify POST or GET request
+        endpoint : str
+            URL endpoint oof API (Does not include base URL)
+        params : dict
+            Dictionary of parameters to be passed with request
+
+        Returns
+        -------
+        Response : Response
+            A requests response object
+        """
         URL = self.APIMainURL + endpoint
 
         if method == "POST":
@@ -18,9 +61,37 @@ class APIRequestor:
             raise Exception(f"Invalid request method: {method}")
 
     def post(self, URL, params=None):
+        """Make a POST request to a given API endpoint
+
+        Parameters
+        ----------
+        URL : str
+            Full URL endpoint of API
+        params : dict
+            Dictionary of parameters to be passed with request
+
+        Returns
+        -------
+        Response : Response
+            A requests response object
+        """
         return self.session.post(URL, params)
 
     def get(self, URL, params=None):
+        """Make a GET request to a given API endpoint
+
+        Parameters
+        ----------
+        URL : str
+            Full URL endpoint of API
+        params : dict
+            Dictionary of parameters to be passed with request
+
+        Returns
+        -------
+        Response : Response
+            A requests response object
+        """
         auth = self.session.headers["Authorization"]
         response = requests.get(URL, headers={"Authorization": auth})
         return response
--- src/wealthsimple_trade_python/wealthsimple.py
+++ src/wealthsimple_trade_python/wealthsimple.py
@@ -1,4 +1,3 @@
-import requests
 from requestor import APIRequestor
 from requests import Session
 import os
@@ -6,8 +5,60 @@ import json
 
 
 class WSTrade:
+    """
+    Wealthsimple Trade API wrapper
+
+    Attributes
+    ----------
+    session : session
+        A requests Session object to be associated with the class
+    APIMainURL : str
+        Main URL endpoint for API
+    TradeAPI : APIRequester
+        APIRequester object to handle API calls
+
+    Methods
+    -------
+    login(email=None, password=None, two_factor_callback=None)
+        Login to Wealthsimple Trade account
+    get_accounts()
+        Get Wealthsimple Trade accounts
+    get_account_ids()
+        Get Wealthsimple Trade account ids
+    get_account(id)
+        Get a Wealthsimple Trade account given an id
+    get_account_history(id, time="all")
+        Get Wealthsimple Trade account history
+    get_activities()
+        Get Wealthsimple Trade activities
+    get_orders(symbol=None)
+        Get Wealthsimple Trade orders 
+    get_security(symbol)
+        Get information about a security
+    get_positions(id)
+        Get positions
+    get_person()
+        Get Wealthsimple Trade person object
+    get_me()
+        Get Wealthsimple Trade user object
+    get_bank_accounts():
+        Get list of bank accounts tied to Wealthsimple Trade account
+    get_deposits()
+        Get list of deposits
+    get_forex()
+        Get foreign exchange rate
+    """
+
     def __init__(self, email, password):
-        self.session = requests.Session()
+        """
+        Parameters
+        ----------
+        email : str
+            Wealthsimple Trade login email
+        password : str
+            Wealthsimple Trade login password
+        """
+        self.session = Session()
         self.APIMAIN = "https://trade-service.wealthsimple.com/"
         self.TradeAPI = APIRequestor(self.session, self.APIMAIN)
         self.login(email, password)
@@ -252,12 +303,12 @@ class WSTrade:
 
 WS = WSTrade(os.environ["WSUSER"], os.environ["WSP"])
 print("\n\n\n\n")
+print(WS.__doc__)
 id = WS.get_account_ids()
 # account = WS.get_account(id[2])
 # history = WS.get_account_history(time="1y", id="lol")
 # me = WS.get_positions(id[2])
 me = WS.get_positions(id=id[2])
-print(me)
 # print(me["results"])
 """
 for x in me:
