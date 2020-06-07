7406e6aaec2fd01805438f40f2bde8cdc3d309b1
Sean Sullivan <seansullivan3@cmail.carleton.ca>
2020-06-07 14:18:10 -0400

    Add docstrings to WSTrade methods

--- src/wealthsimple_trade_python/wealthsimple.py
+++ src/wealthsimple_trade_python/wealthsimple.py
@@ -13,6 +13,19 @@ class WSTrade:
         self.login(email, password)
 
     def login(self, email=None, password=None, two_factor_callback=None):
+        """Login to Wealthsimple Trade account
+
+        Parameters
+        ----------
+        email : str
+            Wealthsimple Trade account email
+        password : str
+            Wealthsimple Trade account password
+
+        Returns
+        -------
+        None
+        """
         if email and password:
             data = [
                 ("email", email),
@@ -35,14 +48,26 @@ class WSTrade:
             raise Exception("Missing login credentials")
 
     def get_accounts(self):
-        """Good"""
+        """Get Wealthsimple Trade accounts
+
+        Returns
+        -------
+        list
+            A list of Wealthsimple Trade account dictionary objects
+        """
         response = self.TradeAPI.makeRequest("GET", "account/list")
         response = response.json()
         response = response["results"]
         return response
 
     def get_account_ids(self):
-        """Good"""
+        """Get Wealthsimple Trade account ids
+
+        Returns
+        -------
+        list
+            A list of Wealthsimple Trade account ids
+        """
         userAccounts = self.get_accounts()
         accountIDList = []
         for account in userAccounts:
@@ -51,7 +76,18 @@ class WSTrade:
         return accountIDList
 
     def get_account(self, id):
-        """Good"""
+        """Get a Wealthsimple Trade account given an id
+
+        Parameters
+        ----------
+        id : str
+            Wealthsimple Trade account id
+
+        Returns
+        -------
+        dict
+            A dictionary containing the Wealthsimple Trade account
+        """
         userAccounts = self.get_accounts()
         for account in userAccounts:
             if account["id"] == id:
@@ -59,7 +95,20 @@ class WSTrade:
         raise NameError(f"{id} does not correspond to any account")
 
     def get_account_history(self, id, time="all"):
-        """Good"""
+        """Get Wealthsimple Trade account history
+
+        Parameters
+        ----------
+        id : str
+            Wealthsimple Trade account id
+        time : str
+            String containing time interval for history
+
+        Returns
+        -------
+        dict
+            A dictionary containing the historical Trade account data
+        """
         response = self.TradeAPI.makeRequest(
             "GET", f"account/history/{time}?account_id={id}"
         )
@@ -71,11 +120,30 @@ class WSTrade:
         return response
 
     def get_activities(self):
+        """Get Wealthsimple Trade activities
+
+        Returns
+        -------
+        list
+            A list of dictionaries containing Wealthsimple Trade activities
+        """
         response = self.TradeAPI.makeRequest("GET", "account/activities")
         response = response.json()
         return response["results"]
 
     def get_orders(self, symbol=None):
+        """Get Wealthsimple Trade orders
+
+        Parameters
+        ----------
+        symbol : str
+            Symbol for security to filter orders on
+
+        Returns
+        -------
+        list
+            A list containing Wealthsimple Trade order dictionaries
+        """
         response = self.TradeAPI.makeRequest("GET", "orders")
         response = response.json()
         # Check if order must be filtered:
@@ -89,11 +157,35 @@ class WSTrade:
             return response
 
     def get_security(self, symbol):
+        """Get information about a security
+
+        Parameters
+        ----------
+        symbol : str
+            Symbol for security to search on
+
+        Returns
+        -------
+        list
+            A list containing matching securities
+        """
         response = self.TradeAPI.makeRequest("GET", f"securities?query={symbol}")
         response = response.json()
         return response["results"]
 
     def get_positions(self, id):
+        """Get positions
+
+        Parameters
+        ----------
+        id : str
+            Wealthsimple Trade account id
+
+        Returns
+        -------
+        list
+            A list containing positions
+        """
         response = self.TradeAPI.makeRequest(
             "GET", f"account/positions?account_id={id}"
         )
@@ -101,28 +193,59 @@ class WSTrade:
         return response["results"]
 
     def get_person(self):
-        """Good"""
+        """Get Wealthsimple Trade person object
+
+        Returns
+        -------
+        dict
+            A dictionary containing a person object
+        """
         response = self.TradeAPI.makeRequest("GET", "person")
         return response.json()
 
     def get_me(self):
-        """Good"""
+        """Get Wealthsimple Trade user object
+
+        Returns
+        -------
+        dict
+            A dictionary containing a user object
+        """
         response = self.TradeAPI.makeRequest("GET", "me")
         return response.json()
 
     def get_bank_accounts(self):
-        """Good"""
+        """Get list of bank accounts tied to Wealthsimple Trade account
+
+        Returns
+        -------
+        list
+            A list of dictionaries containing bank account objects
+        """
         response = self.TradeAPI.makeRequest("GET", "bank-accounts")
         response = response.json()
         return response["results"]
 
     def get_deposits(self):
+        """Get list of deposits
+
+        Returns
+        -------
+        list
+            A list of dictionaries containing deposit objects
+        """
         response = self.TradeAPI.makeRequest("GET", "deposits")
         response = response.json()
         return response["results"]
 
     def get_forex(self):
-        """Good"""
+        """Get foreign exchange rate
+
+        Returns
+        -------
+        dict
+            A dictionary containing foreign exchange rates
+        """
         response = self.TradeAPI.makeRequest("GET", "forex")
         return response.json()
 
