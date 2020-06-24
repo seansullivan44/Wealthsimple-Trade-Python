714b3f808b5fb800aa5c5eac385d483bf695a925
seansullivan44 <46356344+seansullivan44@users.noreply.github.com>
2020-06-24 19:43:38 -0400

    Update conf.py
    Explicitly assign Master Document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+master_doc = 'index'
+
 import os
 import sys
 
